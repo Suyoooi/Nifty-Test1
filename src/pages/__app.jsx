439f3f60b443e139c0e6b2d1cde6b8a99b11aa25
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-19 15:56:48 +0900

    fix: app에 import 주석 처리

--- src/pages/__app.jsx
+++ src/pages/__app.jsx
@@ -1,14 +1,13 @@
+import RootLayout from "@/components/layout/layout";
 import "@/styles/globals.css";
 
 /*---Nifty Css---*/
-import "../public/assets/css/nifty.css";
-import "../../public/assets/js/nifty.js";
+// import "../public/assets/css/nifty.css";
+// import "../../public/assets/js/nifty.js";
 
 /*---Bootstrap Css---*/
-import "bootstrap/dist/css/bootstrap.css";
-import "../public/assets/css/bootstrap.css";
-
-import RootLayout from "@/components/layout/layout";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "../public/assets/css/bootstrap.css";
 
 export default function App({ Component, pageProps }) {
   return (
